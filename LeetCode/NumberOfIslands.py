35b339fe1b7951a89a3eb5947636f3bc17499803
G1 <g1.jeon@kakao.com>
2021-08-02 23:32:30 +0900

    add: LeetCode - DFS - numOfIslands

---
+++ LeetCode/NumberOfIslands.py
@@ -0,0 +1,40 @@
+def numIslands(grid):
+    def dfs(i, j):
+        if i < 0 or i >= len(grid) or \
+           j < 0 or j >= len(grid[0]) or \
+           grid[i][j] != 1:
+            return
+
+        grid[i][j] = 0
+
+        dfs(i - 1, j) # Top
+        dfs(i, j - 1) # Left
+        dfs(i, j + 1) # Right
+        dfs(i + 1, j) # Bottom
+
+    if not grid:
+        return 0
+    
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                dfs(i, j)
+                count += 1
+    
+    return count
+
+if __name__ == "__main__":
+    # grid = [
+    #     [1,1,1,1,0],
+    #     [1,1,0,1,0],
+    #     [1,1,0,0,0],
+    #     [0,0,0,0,0]
+    # ]
+    grid = [
+        [1,1,0,0,0],
+        [1,1,0,0,0],
+        [0,0,1,0,0],
+        [0,0,0,1,1]
+    ]
+    print(numIslands(grid))
